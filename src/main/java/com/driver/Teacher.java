45293dbefc74e5046ef06eb86bf2955625b1ce69
AnthonyVictor24 <arogyaanthonyvictor24@gmail.com>
2024-04-04 14:12:19 +0530

    ee

--- src/main/java/com/driver/Teacher.java
+++ src/main/java/com/driver/Teacher.java
@@ -8,9 +8,9 @@ public class Teacher {
 
     private int age;
 
-    public Teacher() {
-
-    }
+//    public Teacher() {
+//
+//    }
 
     public Teacher(String name, int numberOfStudents, int age) {
         this.name = name;
